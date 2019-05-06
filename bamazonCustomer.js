ab1afa7f3b228b8b54ae8a15f8393d5fef000bab
Kyle Caplos <kylemcaplis@hotmail.com>
2019-05-06 15:00:57 -0400

    purchases available, quantity updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,40 +1,107 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 8889,
-  
-    user: "root",
-  
-    password: "root",
-    
-    database: "bamazon_DB"
-  });
+  host: "localhost",
 
-  connection.connect(function(err) {
+  port: 3306,
+
+  user: "root",
+
+  password: "root",
+
+  database: "bamazon_DB"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId)
+  start();
+
+});
+
+var start = function () {
+  var sqlQuery = "SELECT * FROM products"
+
+  connection.query(sqlQuery, function (err, results) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId)
-    start();
-    
-  });
 
-var start = function(){
-    connection.query("SELECT * FROM products", function(err, results) {
-        if (err) throw err;
-        inquirer
-        .prompt(
-          {
-            name: "product_name",
-            type: "rawlist",
-            choices: function(value) {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].product_name);
-              }
-              return choiceArray;
+    inquirer
+      .prompt(
+        {
+          name: "product_name",
+          type: "rawlist",
+          message: "What is the item you would like to view?",
+          choices: function (value) {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
             }
+            return choiceArray;
+          }
+        }).then(function (answer) {
+          var item = GetItem(results, answer.product_name);
+
+          SelectQuantity(item);
         })
   });
+}
+
+function GetItem(data, item) {
+  //console.log(data)
+  //console.log(item)
+
+  //console.log pingpong ball from data
+  //console.log(data[2].product_name)
+
+  //data[something].product_name === item
+  // return this item
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].product_name === item) {
+      return data[i]
+    }
+  }
+  return null;
+}
+
+function SelectQuantity(dbItem) {
+
+  inquirer.prompt({
+    name: "quantity",
+    type: "input",
+    message: "How many do you want?"
+  }).then(function (answer) {
+    var quantity = parseInt(answer.quantity)
+
+    if (dbItem.stock_quantity < quantity) {
+      // ask them  again how many they want
+
+      console.log("We don't have that many! We have " + dbItem.stock_quantity)
+      SelectQuantity(dbItem)
+    }
+    //else if 
+    //(dbItem.stock_quantity >= quantity){
+    //  console.log("Your purchase is complete!")
+    //}
+    else {
+      // call function that reduces inventory
+      console.log("Your purchase is complete!")
+      reduceInventory(dbItem, quantity)
+    }
+  })
+
+}
+function reduceInventory(item, amount){
+  var sqlQuery = "";
+
+  var totalCost = item.price * amount
+  console.log("$" + totalCost)
+  
+  //connection.query(sqlQuery, function(err, res){
+    //if(err) throw err;
+
+    //console.log(res);
+  //})
 }
